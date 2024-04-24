9e37755e9570cd38c4679678ff9080d6d98d86b2
Azhar Botbayeva <azhar.botbayeva@gmail.com>
2024-04-24 22:23:35 +0100

    add: added get and post views functions

--- chefsTable/littleLemon/views.py
+++ chefsTable/littleLemon/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views import View
 # Create your views here.
 
 def hello(request):
     return HttpResponse("Welcome to Little Lemon restaurant!")
+
+class MyView(View): 
+    def get(self, request): 
+        # logic to process GET request
+        return HttpResponse('response to GET request') 
+ 
+    def post(self, request): 
+        # <logic to process POST request> 
+        return HttpResponse('response to POST request')
