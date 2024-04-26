0a6058ef76c43ce42f417d8e622708616f711d61
Azhar Botbayeva <azhar.botbayeva@gmail.com>
2024-04-26 23:48:34 +0100

    Testing Django Request Response Objects

--- chefsTable/littleLemon/views.py
+++ chefsTable/littleLemon/views.py
@@ -6,11 +6,21 @@ from django.views import View
 def hello(request):
     return HttpResponse("Welcome to Little Lemon restaurant!")
 
-class MyView(View): 
+def index(request):
+    path = request.path 
+    method = request.method 
+    content=''' 
+<center><h2>Testing Django Request Response Objects</h2> 
+<p>Request path : " {}</p> 
+<p>Request Method :{}</p></center> 
+'''.format(path, method) 
+    return HttpResponse(content)
+
+class MyView(View):
     def get(self, request): 
         # logic to process GET request
         return HttpResponse('response to GET request') 
  
     def post(self, request): 
-        # <logic to process POST request> 
+        # <logic to process POST request>
         return HttpResponse('response to POST request')
